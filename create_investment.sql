377e4af2c9ae88f10edf0aa9bcc93d3d61ba89aa
jnmathew <93227841+jnmathew@users.noreply.github.com>
2023-12-02 15:01:27 -0500

    Update create_investment.sql

--- create_investment.sql
+++ create_investment.sql
@@ -12,6 +12,14 @@ BEGIN
     IF symbol_p IS NULL OR company_name_p IS NULL OR daily_value_p THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Date and number of shares must not be null.';
 
+    -- if CASH is set to a value other than 1
+    ELSEIF symbol_p = "CASH" and daily_value_p != 1 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'CASH must be created with value 1.';
+
+    -- if DEBT is set to a value other than -1
+    ELSEIF symbol_p = "DEBT" and daily_value_p != -1 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'DEBT must be created with value -1.';
+    
     -- if non-debt investment is negative
     ELSEIF symbol_p != "DEBT" and daily_value_p < 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This investment cannot be negative.';
@@ -19,7 +27,7 @@ BEGIN
     END IF;
 
     -- create the investment
-    INSERT INTO investments(symbol, company_name, daily_value)
+    INSERT INTO investments(symbol, company_name, daily_value) 
     VALUES (symbol_p, company_name_p, daily_value_p);
 END $$
 
